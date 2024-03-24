708c09e33a8340bc1a8b637b764ab025323d16a3
annieriya <riyaanu45@gmail.com>
2024-03-24 14:25:28 +0530

    changes for actions class

--- src/actions.java
+++ src/actions.java
@@ -12,10 +12,10 @@ public static void main (String[] args)
 	System.setProperty("webdriver.chrome.driver", "/Users/ANNIE/Documents/chromedriver/chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
-	Actions a = new Actions(driver);
+	Actions act = new Actions(driver);
 	WebElement move= driver.findElement(By.id("nav-link-accountList-nav-line-1"));
-	a.moveToElement(move).contextClick().build().perform()
-	a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("mobile").doubleClick().build().perform();
+	act.moveToElement(move).contextClick().build().perform()
+	act.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("mobile").doubleClick().build().perform();
 
 	}
 	}
