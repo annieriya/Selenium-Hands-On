48008302a219c504712f037373577d843a8d0e60
annieriya <riyaanu45@gmail.com>
2024-03-24 12:50:28 +0530

    changes for arraystream

--- src/convertarraystreams.java
+++ src/convertarraystreams.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 public class convertarraystreams {
 public static void main (String[] args) {
 	//Stream.of("Annie", "Balram" , "Danny").filter(s  ->s.startsWith("a")).forEach(s-> System.out.println(s));
-	List<String> name = Arrays.asList("Annie","Balram","Danny");
+	List<String> name = Arrays.asList("Annie","Canberra","Danny");
 	name.stream().filter(s->s.startsWith("a")).sorted().map(s->s.toUpperCase()).forEach(s-> System.out.println(s));
 }
 }
